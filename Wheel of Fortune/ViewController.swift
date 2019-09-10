aa5bc1d2599bb8073ef1114286225c032055cc92
Вячеслав Корниенко <s@webant.ru>
2019-09-10 23:06:04 +0300

    here we go

---
+++ Wheel of Fortune/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Wheel of Fortune
+//
+//  Created by admin on 9/10/19.
+//  Copyright © 2019 Slava Kornienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
